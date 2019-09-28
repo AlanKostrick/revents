6e3620c6fca31c71ef34a1c5324c9a5065a6401c
AlanKostrick <alan@wecancodeit.org>
2019-09-27 22:14:52 -0500

    setup form

--- src/features/event/eventList/EventListAttendee.jsx
+++ src/features/event/eventList/EventListAttendee.jsx
@@ -3,14 +3,11 @@ import { List, Image } from "semantic-ui-react";
 
 class EventListAttendee extends Component {
   render() {
+    //destructuring so that you do not need this.props.attendee.photoURL
+    const { attendee } = this.props;
     return (
       <List.Item>
-        <Image
-          as='a'
-          size='mini'
-          circular
-          src='https://randomuser.me/api/portraits/women/42.jpg'
-        />
+        <Image as='a' size='mini' circular src={attendee.photoURL} />
       </List.Item>
     );
   }
